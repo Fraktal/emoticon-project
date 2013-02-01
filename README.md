e81ea823638196afc979a1d920260fc414a1f11f
Todd Ervin <servin@unca.edu>
2013-01-31 23:22:37 -0500

    re-commit to fix inconsistent files and modules: re-commit 1-30-13

---
+++ README.md
@@ -0,0 +1,3 @@
+The emoticon-project is a sentiment analysis web app using machine learning methods such as K-means and K nearest neighbor to link tweets with emoticons to track sentiment
+
+This code is being developed by the NINA Lab at the University of North Carolina Asheville
---
+++ app/tracker.js
@@ -0,0 +1,24 @@
+var twitter = require("immortal-ntwitter")
+var express = require("express");
+var app = express();
+var credentials = require('./credentials.js');
+
+var twit = new twitter({
+    consumer_key: credentials.consumer_key,
+    consumer_secret: credentials.consumer_secret,
+    access_token_key: credentials.access_token_key,
+    access_token_secret: credentials.access_token_secret
+});
+
+app.listen(3000);
+
+twit.immortalStream('statuses/sample', null, function(immortalStream) {
+      immortalStream.on('data', function(data){
+         var d = (data.created_at);
+         var month = new Date(Date.parse(d)).getMonth()+1;
+         var day = new Date(Date.parse(d)).getDate();
+         var year = new Date(Date.parse(d)).getFullYear();
+         var date = (year + "-" + month + "-" + day);
+         console.log(date + ' The tweets are '+ data.text.match(/\s(\w+)\s((?::|;|=)(?:-)?(?:\)|D|P))/));
+       });         
+   });
