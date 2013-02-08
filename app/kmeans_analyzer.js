a9d5ce59726deaa418e9fc2b370b96eaa37276fb
Todd Ervin <servin@unca.edu>
2013-02-07 21:03:49 -0500

    updated package.json

--- app/kmeans_analyzer.js
+++ app/kmeans_analyzer.js
@@ -1,4 +1,4 @@
-var kmeans = require('node-kmeans');
+var kmeans = require('kmeans');
 var dburl = 'localhost/querydb';
 var collections = ['querydata'];
 var db = require('mongojs').connect(dburl,collections);
@@ -9,6 +9,15 @@ var clusterData = db.querydata.find({tweet: 1}, function(err, querydata){
      console.log(querydata);
      });
 });
+
+/*var vectors = new Array();
+for (var i = 0 ; i < data.length ; i++)
+  vectors[i] = [ data[i]['data'] , data[i]['data']];
+
+kmeans.clusterize(vectors, {k: 4}, function(err,res) {
+  if (err) console.error(err);
+  else console.log('%o',res);
+});*/
 
 // this is keeps throwing the err "tweet not found"
 // Once we get the this working, we can start coding the Kmeans algo
