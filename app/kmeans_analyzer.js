ab35c24bffcf9fe67bb8a51cbdbd3e0326772a6e
Todd Ervin <servin@unca.edu>
2013-02-11 10:58:22 -0500

    getting the error [Error: The number of points must be greater than the number k of clusters]

--- app/kmeans_analyzer.js
+++ app/kmeans_analyzer.js
@@ -5,14 +5,27 @@ var db = require('mongojs').connect(dburl,collections);
 
 var data = db.querydata.find().sort({tweet: 1}, function(err, data) {
     if(err)console.log('DATA NOT FOUND: ' + err); 
-    else console.log(data);
+    //else console.log(data);
 });
- 
-/*var vectors = new Array();
-for (var i = 0 ; i < data.length ; i++)
-  vectors[i] = [ data[i]['data'] , data[i]['data']];
+
+var count = 0;
+var key;
+for(key in data){
+    var value = data[key];
+    count++;
+}
+
+  // Create the labels and the vectors describing the data
+
+var labels = new Array ;
+var vectors = new Array ;
+  for (var i = 0 ; i < key ; i++) {
+      labels[i] = data[i]['tweet'] ;
+      vectors[i] = [ data[i]['smiley'] , data[i]['sad'], data[i]['neutral']] ;
+}
 
 kmeans.clusterize(vectors, {k: 4}, function(err,res) {
   if (err) console.error(err);
   else console.log('%o',res);
-});*/
+});
+
